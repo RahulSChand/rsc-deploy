85a559dd3d6432711ece542c1328fdbbc034845b
Rahul Chand <t-rahulchand@microsoft.com>
2023-03-04 16:53:03 +0530

    next config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 
 const nextConfig = {
-  assetPrefix: isProd ? '/rsc-pages/' : '',
+  assetPrefix: isProd ? '/rsc-deploy/' : '',
   images: {
     unoptimized: true,
   },
