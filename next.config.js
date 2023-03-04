78793ba891c6df92a2487726a790f879c7ff771c
Rahul Chand <t-rahulchand@microsoft.com>
2023-03-04 17:12:37 +0530

    config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-/** @type {import('next').NextConfig} */
-
 const isProd = process.env.NODE_ENV === 'production'
 
 
@@ -11,5 +9,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-//ssh-keygen -t rsa -b 4096 -C "$(git config user.email)" -f rsc-pages -N ""
